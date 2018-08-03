e819a72a467565787e6d9863d1ca0772b82422ae
Seth Walton <sdwalton83@gmail.com>
2018-08-02 22:51:07 -0500

    quick egg timer no text input yet

--- Egg Timer/ViewController.swift
+++ Egg Timer/ViewController.swift
@@ -9,7 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var timer = Timer()
+    
+    var time = 210
+    
+    @objc func decreaseTimer(){
+        
+        if time > 0{
+            time -= 1
+            
+            timerLabel.text = String(time)
+        } else {
+            timer.invalidate()
+        }
+        
+        
+    }
+    
+    @IBOutlet var timerLabel: UILabel!
+    
+    @IBAction func play(_ sender: Any) {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.decreaseTimer), userInfo: nil, repeats: true)
+    }
+    
+    @IBAction func pause(_ sender: Any) {
+        timer.invalidate()
+    }
+    
+    
+    @IBAction func plus10(_ sender: Any) {
+        time = time + 10
+    }
+    
+    @IBAction func minus10(_ sender: Any) {
+        time = time - 10
+    }
+    
+    @IBAction func resetTimer(_ sender: Any) {
+        time = 210
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
